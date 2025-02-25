e3ff134fd82ae4d371903c5b11d791b0ac08c7f7
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-02-25 14:16:59 -0500

    Update Math Operators.py

--- Math Operators.py
+++ Math Operators.py
@@ -5,3 +5,33 @@ print (2.2+2.2)
 #this is boolean, a true or false statement
 logged_in=True 
 #this is char, a single character
+charA=('a')
+#this is string, a combination of characters
+str=("Hello World")
+
+#this is input
+print ("Please enter your name")
+name=input()
+
+#this is multiplication
+print (2*3)
+
+#this is division
+print (5/2)
+
+#this is addtion 
+print (2+3)
+
+#this is subtraction
+print (3-2)
+
+#this is modulus, this shows the remainder when you divide
+print (23/4)
+
+#this is to the power of. ** is used for exponents
+print (2**3)
+
+import math
+#this is sqr root, you have to use import math to use it
+print(math.sqrt(9))
+
