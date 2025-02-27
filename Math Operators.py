834ef7148c721c355b8c6899c4a8a5fc2cddf601
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-02-27 14:18:13 -0500

    Update Math Operators.py

--- Math Operators.py
+++ Math Operators.py
@@ -15,33 +15,33 @@ str=("Hello World")
 print ("String Example:",str)
 
 #this is input
-print ("Please enter your name")
+print ("Please enter your name:")
 name=input()
 
 #this is multiplication
 product=2*3
-print ("the product is",product)
+print ("the product is:",product)
 
 
 #this is division 
 quotient=4/2
-print ("the quotient is",quotient)
+print ("the quotient is:",quotient)
 
 #this is addtion 
 sum=2+5
-print ("the sum is",sum)
+print ("the sum is:",sum)
 
 #this is subtraction
 difference=10-2
-print ("the difference is",difference)
+print ("the difference is:",difference)
 
 #this is modulus, this shows the remainder when you divide
 remainder=(23%4)
-print ("the remainder is",remainder)
+print ("the remainder is:",remainder)
 
 #this is to the power of. ** is used for exponents
 exponent=(2**3)
-print ("the exponent is",exponent)
+print ("the exponent is:",exponent)
 
 
 import math
@@ -58,21 +58,21 @@ c=float(input("Type the value of c, then press Enter "))
 
 #Formula of discriminant
 delta = b**2 - 4*a*c
-print("The discriminant is",delta)
+print("The discriminant is:",delta)
 
 #3D Volume
 
 #Cube
 
 #input from user, we have to cast the input for all the shapes as its a string otherwise
-cube_side=float(input("Enter A(lenght of edge), then press Enter "))
+cube_side=float(input("Enter A(lenght of edge), then press Enter:"))
 
 #formula of volume of cube
 
 cube_volume= cube_side**3
 
 #shows the volume of cube
-print("The volume of the cube is",cube_volume)
+print("The volume of the cube is:",cube_volume)
 
 #Sphere
 
@@ -82,7 +82,7 @@ sphere_radius= float(input("Enter r(radius), then press Enter "))
 #formula of volume of sphere
 sphere_volume= (4/3) * (math.pi) * (sphere_radius**3)
 #shows the volume of sphere
-print("The volume of the sphere is",sphere_volume)
+print("The volume of the sphere is:",sphere_volume)
 
 
 #Cone
@@ -93,7 +93,7 @@ cone_height= float(input ("Enter h(height), then press Enter"))
 cone_volume= (1/3) * (math.pi) * (cone_radius*2) * (cone_height)
 
 #shows the volume of cone
-print("The volume of the cone is", cone_volume)
+print("The volume of the cone is:", cone_volume)
 
 
 #Cylinder 
@@ -104,4 +104,4 @@ cylinder_height= float(input ("Enter h(height), then press Enter"))
 cylinder_volume= (math.pi) * (cylinder_radius**2) * (cylinder_height)
 
 #shows the volume of cylinder
-print("The volume of a cylinder is", cylinder_volume)
+print("The volume of a cylinder is:", cylinder_volume)
