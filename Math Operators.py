cb075794d10b5d9f7f7b9c96ab97dbd1f39546c6
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-02-27 13:30:13 -0500

    Update Math Operators.py

--- Math Operators.py
+++ Math Operators.py
@@ -64,29 +64,44 @@ print("The discriminant is",delta)
 
 #Cube
 
+#input from user, we have to cast the input for all the shapes as its a string otherwise
 cube_side=float(input("Enter A(lenght of edge), then press Enter "))
+
+#formula of volume of cube
+
 cube_volume= cube_side**3
-print("The volume of cube is",cube_volume)
+
+#shows the volume of cube
+print("The volume of the cube is",cube_volume)
 
 #Sphere
 
+#input from user
 sphere_radius= float(input("Enter r(radius), then press Enter "))
+
+#formula of volume of sphere
 sphere_volume= (4/3) * (math.pi) * (sphere_radius**3)
-print("The volume of a sphere is",sphere_volume)
+#shows the volume of sphere
+print("The volume of the sphere is",sphere_volume)
 
 
 #Cone
 
-
+#input from user
 cone_radius= float(input("Enter r(radius), then press Enter"))
 cone_height= float(input ("Enter h(height), then press Enter"))
 cone_volume= (1/3) * (math.pi) * (cone_radius*2) * (cone_height)
-print("The volume of a cone is", cone_volume)
+
+#shows the volume of cone
+print("The volume of the cone is", cone_volume)
 
 
 #Cylinder 
 
+#input from user
 cylinder_radius= float(input("Enter r(radius), then press Enter"))
 cylinder_height= float(input ("Enter h(height), then press Enter"))
 cylinder_volume= (math.pi) * (cylinder_radius**2) * (cylinder_height)
+
+#shows the volume of cylinder
 print("The volume of a cylinder is", cylinder_volume)
